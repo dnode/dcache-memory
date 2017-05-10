ee18384f4f648fbe70f619a44fcf35b6a2009a66
Christoph Herrmann <christoph.herrmann@maxdome.de>
2017-05-10 17:19:41 +0200

    dcache-memory

---
+++ README.md
@@ -0,0 +1,13 @@
+[![Dependency Status](https://david-dm.org/dnode/dcache-memory/status.svg)](https://david-dm.org/dnode/dcache-memory)
+[![devDependency Status](https://david-dm.org/dnode/dcache-memory/dev-status.svg)](https://david-dm.org/dnode/dcache-memory?type=dev)
+
+# Installation
+
+`npm i --save dcache-memory`
+
+
+# Initialisation
+
+```javascript
+const client = require('dcache-memory')();
+```
---
+++ src/lib.js
@@ -0,0 +1,20 @@
+module.exports = () => {
+  const memory = {};
+  return {
+    expire: (key, expire) => {
+      setTimeout(
+        () => {
+          delete memory[key];
+        },
+        expire * 1000
+      )
+    },
+    getJSON: (key, callback) => {
+      callback(null, memory[key]);
+    },
+    setJSON: (key, value, callback) => {
+      memory[key] = value;
+      callback();
+    },
+  };
+};
